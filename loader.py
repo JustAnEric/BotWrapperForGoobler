833ec7f104ddb41a86e70df3348836f019aaa427
EricPlayz YT <73326807+WWEMGamer2@users.noreply.github.com>
2022-03-27 01:10:38 +0000

    Updated python package - failing to load

--- loader.py
+++
@@ -1,5 +0,0 @@
-import time
-def load():
-  print("Loading the GooblerAPI...")
-  time.sleep(4)
-  return print("Error in API.page \n Unknown: The package is not available right now. Please uninstall the package to continue.")
