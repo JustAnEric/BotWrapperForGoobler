a2353d474fcc1102c464536ba01b8d88e91895f9
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2022-03-27 11:54:09 +1100

    Create loader.py

---
+++ loader.py
@@ -0,0 +1,5 @@
+import time
+def load():
+  print("Loading the GooblerAPI...")
+  time.sleep(4)
+  return print("Error in API.page \n Unknown: The package is not available right now. Please uninstall the package to continue.")
